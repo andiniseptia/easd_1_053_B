d66575d37c8d4bc424e8f6fbc820653241504f13
andiniseptia <andiniseptiandri124@gmail.com>
2023-04-07 10:31:23 +0700

    Jawaban essay 1-5

---
+++ easd_1_053_B/easd_1_053_B.cpp
@@ -0,0 +1,13 @@
+//1. Karena algoritma dapat membantu dalam membuat langkah-langkah yang terstruktur untuk menyelesaikan masalah, 
+//memungkinkan untuk melakukan analisis dan perbaikan perangkat lunak secara efektif, 
+//membantu dalam mengotomatiskan tugas-tugas yang berulang dan kompleks,
+//menjamin konsistensi dalam penyelesaian masalah dan mengurangi kesalahan manusia, 
+//dan membantu dalam menghemat waktu dan biaya dalam menyelesaikan masalah.
+//2. Statis dan dinamis.
+//3. Kecepatan mesin, Compiler(mengubah bahasa kita menjadi Bahasa komputer), Sistem operasi, Bahasa pemrograman,Ukuran input.
+//4. Algoritma Quicksort. Kelebihan dari algoritma ini adalah kinerjanya yang sangat cepat dibanding
+//algoritma pengurutan data lainnya, bahkan untuk data yang berukuran besar. 
+//Quicksort memanfaatkan metode divide and conquer yang memungkinkan untuk mengurangi kompleksitas waktu secara drastis.
+//Oleh karena itu, quicksort lebih efisien dan mudah diimplementasikan untuk data berukuran besar.
+//5. Quadratic : Bubblesort, Selectionsort, Insertionsort, dan Shellsort.
+//Loglinear : Quicksort dan Mergesort.
